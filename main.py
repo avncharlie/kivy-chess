9d5604b8359aed6a1f4154c362d1d0c15c738cb4
Alvin Joy Charles <alvinjoycharles@gmail.com>
2016-12-22 14:39:30 +1100

    Added Chess cells programatically

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from kivy.clock import Clock
 
 from kivy.core.window import Window
 
+from kivy.lang import Builder
+
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button 
@@ -11,14 +13,15 @@ from kivy.uix.button import Button
 from kivy.properties import NumericProperty
 
 class Chessboard(GridLayout):
-    def on_size(self, *arg):
+
+    def on_size(self, *args):
         board_dimensions = sorted([self.width, self.height])[0]
 
         self.row_force_default = True
         self.col_force_default = True
 
-        self.row_default_height = board_dimensions/8
-        self.col_default_width = board_dimensions
+        self.row_default_height = board_dimensions/self.rows
+        self.col_default_width = board_dimensions/self.columns
 
 
 class ChessboardCentered(BoxLayout):
@@ -27,12 +30,27 @@ class ChessboardCentered(BoxLayout):
         self.padding = [(self.width-board_dimensions)/2, 
             (self.height-board_dimensions)/2, 0, 0]
 
+class ChessCell(Button):
+    pass
+
 class ChessGame(BoxLayout):
     pass
 
 class ChessboardApp(App):
+    def test(self, *args):
+        print("YEAAH BOI")
     def build(self):
-        return ChessGame()
+        game = ChessGame()
+
+        for child in game.children:
+                if type(child) == ChessboardCentered:
+                    board = child.children[0]
+
+        for num in range(64):
+            button = ChessCell(id=str(num))
+            board.add_widget(button)
+        
+        return game
 
 if __name__ == '__main__':
     ChessboardApp().run()
