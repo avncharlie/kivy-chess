3983f3da672a689b921adeebda8db000831c861d
Alvin Joy Charles <alvinjoycharles@gmail.com>
2016-12-22 15:10:41 +1100

    added chess peice vectors

--- chess engine/chess_with_python.py
+++ chess engine/chess_with_python.py
@@ -2,26 +2,24 @@ import chess
 import chess.uci
 import chess.svg
 import sys
-import StringIO
 
 engine = chess.uci.popen_engine("stockfish")
 board = chess.Board()
-out = StringIO.StringIO()
 
 def print_board(board, flipped=True):
     print(board)
-    f = open("chess.svg", "wr")
+    f = open("chess.svg", "w+")
     f.write(chess.svg.board(board=board, flipped=flipped))
 
 def engine_move(board):
     engine.isready() 
     engine.position(board)
-    engine_move = engine.go(movetime=5000)[0]
+    engine_move = engine.go(movetime=1000)[0]
     board.push(engine_move)
     return board
 
 def player_move(board):
-    player_move = raw_input("Enter move: ")
+    player_move = input("Enter move: ")
     board.push_san(player_move)
     return board
 
